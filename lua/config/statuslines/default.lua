e8171705f0ccc140f6f129d06587993a6b82404a
Nikita <Poni22poni23@yandex.ru>
2026-05-23 12:17:21 +0300

    feat: default statusline rework

--- lua/config/statuslines/default.lua
+++ lua/config/statuslines/default.lua
@@ -2,6 +2,7 @@ vim.opt_global.statusline = table.concat({
     " %f",          -- file name / path
     " %h%m%r",      -- help / modified / readonly flags
     " %=",          -- right align the rest
-    " %(%l,%c%V%)", -- cursor line, col (virt col)
-    " %P",          -- percent through file
+    " %(%l/%L,%c%V%)", -- cursor line, col (virt col)
+    -- selected chars in visual mode
+    "%{mode() =~# '[vV\\^V]' ? ',' .. wordcount().visual_chars : ''}",
 })
