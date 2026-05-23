ddc8f44741759f04a36eda4352e4ae5e4bbd9c79
Nikita <Poni22poni23@yandex.ru>
2026-05-23 12:23:03 +0300

    fix: rework tabs keymaps

--- lua/config/keymaps/tabs.lua
+++ lua/config/keymaps/tabs.lua
@@ -1,9 +1,9 @@
 -- Creating
-vim.keymap.set('n', '<leader>tn', ':tabnew ')
+vim.keymap.set('n', '<leader>tn', ':tabnew _<CR>')
 
 -- Closing
 vim.keymap.set('n', '<leader>tc', ':tabc<CR>')
 
 -- Moving
-vim.keymap.set('n', '<leader>th', ':tabprevious<CR>')
-vim.keymap.set('n', '<leader>tl', ':tabnext<CR>')
+vim.keymap.set('n', '<A-H>', ':tabprevious<CR>')
+vim.keymap.set('n', '<A-L>', ':tabnext<CR>')
