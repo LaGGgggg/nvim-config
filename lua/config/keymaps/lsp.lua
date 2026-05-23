d728ddb2a8822c62fe8ffd836d92399f2d258d03
Nikita <Poni22poni23@yandex.ru>
2026-05-23 12:20:29 +0300

    feat: lsp keymaps rework

--- lua/config/keymaps/lsp.lua
+++ lua/config/keymaps/lsp.lua
@@ -9,21 +9,33 @@ return function()
     vim.keymap.set('n', 'Lh', vim.lsp.buf.hover, { buffer = true })
     vim.keymap.set('n', 'LH', vim.lsp.buf.signature_help, { buffer = true })
 
-    -- Find/rename
-    vim.keymap.set('n', 'Lf', vim.lsp.buf.references, { buffer = true })
+    -- Actions
     vim.keymap.set('n', 'Lr', vim.lsp.buf.rename, { buffer = true })
-
-    -- Code actions
+    vim.keymap.set('n', 'Lf', vim.lsp.buf.references, { buffer = true })
     vim.keymap.set('n', 'La', vim.lsp.buf.code_action, { buffer = true })
+    vim.keymap.set(
+        'n',
+        'LI',
+        function()
+            vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                    only = { 'source.organizeImports' },
+                },
+            })
+        end,
+        { buffer = true }
+    )
 
     -- Diagnostics
     vim.keymap.set('n', 'Le', vim.diagnostic.open_float, { buffer = true })
-    vim.keymap.set('n', 'Ln', vim.diagnostic.goto_next, { buffer = true })
-    vim.keymap.set('n', 'Lb', vim.diagnostic.goto_prev, { buffer = true })
+    vim.keymap.set('n', 'Lj', vim.diagnostic.goto_prev, { buffer = true })
+    vim.keymap.set('n', 'Lk', vim.diagnostic.goto_next, { buffer = true })
 
     -- Situational
     vim.keymap.set('n', 'LR', vim.diagnostic.setloclist, { buffer = true })
     vim.keymap.set('n', 'LF', function()
         vim.lsp.buf.format({ async = true })
     end)
+    vim.keymap.set('n', 'LU', ':lua vim.diagnostic.reset(nil, 0)<CR>mq:e %<CR>', { buffer = true })
 end
