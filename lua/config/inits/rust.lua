aebef64f4b0b8ac8a867495f772117da96f7c95c
Nikita <Poni22poni23@yandex.ru>
2026-05-23 12:19:56 +0300

    feat: rust init

---
+++ lua/config/inits/rust.lua
@@ -0,0 +1,9 @@
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'rust',
+    callback = function()
+        vim.api.nvim_set_option_value('tabstop', 4, { buf = 0 })
+        vim.api.nvim_set_option_value('softtabstop', 4, { buf = 0 })
+        vim.api.nvim_set_option_value('shiftwidth', 4, { buf = 0 })
+        require('virt-column').setup_buffer(0, { virtcolumn = '101' })
+    end,
+})
